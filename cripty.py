4fdaf56cd655c206556f4846a1f5efd8c23421e7
JuliaZoffoli <julia.zoffoli@estudante.ufjf.br>
2024-05-22 14:38:40 -0300

    Cripty

--- cripty.py
+++ cripty.py
@@ -2,44 +2,44 @@
 # Júlia Zóffoli
 
 import abc
+import rsa
 
 class Cripty:
     def __init__(self) -> None:
-        pass
+        self.public_key, self.private_key = rsa.newkeys(1024)
 
-    @abc.abstractmethod
-    def encrypt(text, private_key) -> str:
+    @abc.abstractmethod 
+    def encrypt(text, public_key) -> str:
         '''
-        Encrypts a text using the private key
+        Encrypts a text using the public key
         
         :param text: str
             The text to be encrypted
             
-        :param private_key: str
-            The private key to be used in the encryption
+        :param public_key: str
+            The public key to be used in the encryption
             
         :return: str
             The encrypted text
-            
         '''
-        pass
+        message = text.encode('utf8')
+        return rsa.encrypt(message, public_key)
 
     @abc.abstractmethod
-    def decrypt(text, public_key) -> str:
+    def decrypt(text, private_key) -> str:
         '''
-        Decrypts a text using the public key
+        Decrypts a text using the private key
         
         :param text: str
             The text to be decrypted
         
-        :param public_key: str
-            The public key to be used in the decryption
+        :param private_key: str
+            The private key to be used in the decryption
         
         :return: str
-        
         '''
-        pass
-
+        return rsa.decrypt(text, private_key).decode('utf8')
+        
     @abc.abstractmethod
     def generate_keys() -> tuple:
         '''
@@ -49,4 +49,13 @@ class Cripty:
             A tuple containing the public and private keys
         
         '''
-        pass
+        return rsa.newkeys(1024)
+
+if __name__ == "__main__":
+    rsa_ = Cripty()
+    message = "Júlia Zoffoli"
+    aux = Cripty.encrypt(message,rsa_.public_key)
+    print(aux)
+
+    aux2 = Cripty.decrypt(aux, rsa_.private_key)
+    print(aux2)
